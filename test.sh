50b0c044ec80346ea64bff1fe5acf3697575675f
raphael <raphael.pastre@gmail.com>
2024-06-23 10:27:14 +0200

    Filter target container by name

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ echo "Building and starting services"
 docker compose -f compose-test.yaml up -d
 
 echo "Running tests"
-docker exec -it docker-ide-hot-reload-web-testing-1 python -m unittest discover
+docker exec -it $(docker ps --filter "name=web-testing" --format "{{.ID}}") python -m unittest discover
 
 echo "Stopping services"
 docker compose -f compose-test.yaml down --rmi all -v
